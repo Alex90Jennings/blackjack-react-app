66257c2f443bfdc515c9dfe2291ec4138b2b7e8e
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-05-24 20:14:11 +0200

    presentation

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,10 @@ function Header() {
   return (
     <header className="three-column dashed-border-black three-columns-expand-one-three">
       <div className="dashed-border-pink"></div>
-      <h1 className="dashed-border-blue">$$$ BOOLEAN BLACKJACK $$$</h1>
+      <h1 className="dashed-border-blue">
+        <span className="gold">$$$</span> BOOLEAN BLACKJACK{" "}
+        <span className="gold">$$$</span>
+      </h1>
       <div className="dashed-border-pink"></div>
     </header>
   );
